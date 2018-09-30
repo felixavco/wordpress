f8e2e124ac9bb3bcf2857e7e0b329598a2ec6460
Felix Avelar <felixavco@gmail.com>
2018-09-29 23:59:39 -0600

    updated 201809292359

---
+++ inc/search-route.php
@@ -0,0 +1,136 @@
+<?php
+
+add_action('rest_api_init', 'univeristyRegisterSearch');
+
+function univeristyRegisterSearch()
+{
+    register_rest_route('university/v1', 'search', array(
+        'methods' => WP_REST_SERVER::READABLE,
+        'callback' => 'universitySearchResults',
+    ));
+}
+
+function universitySearchResults($data)
+{
+    $mainQuery = new WP_QUERY(array(
+        'post_type' => array('post', 'page', 'professor', 'program', 'event', 'campus'),
+        's' => sanitize_text_field($data['term']),
+    ));
+
+    $results = array(
+        'generalInfo' => array(),
+        'professors' => array(),
+        'programs' => array(),
+        'events' => array(),
+        'campuses' => array(),
+    );
+
+    while ($mainQuery->have_posts()) {
+        $mainQuery->the_post();
+
+        if (get_post_type() == 'post' or get_post_type() == 'page') {
+            array_push($results['generalInfo'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'postType' => 'post',
+                'authorName' => get_the_author(),
+            ));
+        }
+
+        if (get_post_type() == 'professor') {
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(0, 'pofessorLandscape')
+            ));
+        }
+
+        if (get_post_type() == 'program') {
+          $relatedCampuses = get_field('related_campus');
+
+          if($relatedCampuses){
+            foreach($relatedCampuses as $campus){
+              array_push($results['campuses'], array(
+                'title' => get_the_title($campus),
+                'permalink' => get_the_permalink($campus)
+              ));
+            }
+          }
+         
+
+            array_push($results['programs'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'id' => get_the_id()
+            ));
+        }
+
+        if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
+            array_push($results['events'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'month' => $eventDate->format('M'),
+                'day' => $eventDate->format('d'),
+                'description' => wp_trim_words(get_the_content(), 15)
+            ));
+        }
+
+        if (get_post_type() == 'campus') {
+            array_push($results['campuses'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+        }
+
+    }
+
+    //this code is to add the relationship to the search results
+
+    if ($results['programs']) {
+        $programsMetaQuery = array('relation' => 'OR');
+
+        foreach ($results['programs'] as $item) {
+            array_push($programsMetaQuery, array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . $item['id'] . '"',
+            ));
+        }
+
+        $programRelationShipQuery = new WP_Query(array(
+            'post_type' => array('professor', 'event'),
+            'meta_query' => $programsMetaQuery,
+        ));
+
+        while ($programRelationShipQuery->have_posts()) {
+            $programRelationShipQuery->the_post();
+
+            if (get_post_type() == 'event') {
+                $eventDate = new DateTime(get_field('event_date'));
+                array_push($results['events'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    'month' => $eventDate->format('M'),
+                    'day' => $eventDate->format('d'),
+                    'description' => wp_trim_words(get_the_content(), 15),
+                ));
+            }
+
+            if (get_post_type() == 'professor') {
+                array_push($results['professors'], array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    'image' => get_the_post_thumbnail_url(0, 'pofessorLandscape'),
+                ));
+            }
+
+        }
+
+        $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+        $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
+
+    }
+
+    return $results;
+}
--- js/modules/Search.js
+++ js/modules/Search.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 
 class Search {
   constructor() {
+    this.addSearchHTML();
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
@@ -32,7 +33,7 @@ class Search {
           this.resultDiv.html('<div class="spinner-loader"></div>');
           this.isSpinnerVisible = true;
         }
-        this.typingTimer = setTimeout(this.getReults.bind(this), 1000);
+        this.typingTimer = setTimeout(this.getReults.bind(this), 750);
       } else {
         this.resultDiv.html("");
         this.isSpinnerVisible = false;
@@ -42,15 +43,64 @@ class Search {
   }
 
   getReults() {
-    $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search='+ this.searchField.val(), posts => {
-      this.resultDiv.html(`
-        <h2 class="search-overlay__section-title">General Information </h2>
-        ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matched your search</p>'}
-          ${posts.map(post => `<li><a href="${post.link}">${post.title.rendered}</a></li>`).join('')}
-        ${posts.length ? '</ul>' : ''}
-      `);
-      this.isSpinnerVisible = false;
-    });
+    $.getJSON(`${universityData.root_url}/wp-json/university/v1/search?term=${this.searchField.val()}`, 
+      results => {
+        this.resultDiv.html(`
+          <div class="row">
+            <div class="one-third">
+              <h2 class="search-overlay__section-title">General Information</h2>
+              ${results.generalInfo.length ? '<ul class="link-list min-list">' : '<p>No General Information match your search</p>'}
+              ${results.generalInfo.map(item => `<li><a href="${item.permalink}">${item.title}</a> ${item.authorName ? `<small>by ${item.authorName}</small>`: ''}</li>`).join('')}
+              ${results.generalInfo.length ? '</ul>' : ''}
+            </div>
+
+            <div class="one-third">
+              <h2 class="search-overlay__section-title">Programs</h2>
+              ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No Programs match your search <a href="${universityData.root_url}/programs">View all Programs</a></p>`}
+              ${results.programs.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join('')}
+              ${results.programs.length ? '</ul>' : ''}
+
+              <h2 class="search-overlay__section-title">Professors</h2>
+              ${results.professors.length ? '<ul class="professor-cards">' : `<p>No Professors match your search</p>`}
+              ${results.professors.map(item => `
+                <li class="professor-card__list-item">
+                  <a class="professor-card" href="${item.permalink}">
+                    <img class="professor-card__image" src="${item.image}" alt="${item.title}">
+                    <span class="professor-card__name">${item.title}</span>
+                  </a>
+                </li>
+              `).join('')}
+              ${results.professors.length ? '</ul>' : ''}
+
+            </div>
+
+            <div class="one-third">
+              <h2 class="search-overlay__section-title">Campuses</h2>
+              ${results.campuses.length ? '<ul class="link-list min-list">' : `<p>No Campuses match your search <a href="${universityData.root_url}/campuses">View all Campuses</a></p>`}
+              ${results.campuses.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join('')}
+              ${results.campuses.length ? '</ul>' : ''}
+
+              <h2 class="search-overlay__section-title">Events</h2>
+              ${results.events.length ? '' : `<p>No Events match your search <a href="${universityData.root_url}/events">View all Events</a></p>`}
+              ${results.events.map(item => `
+                <div class="event-summary">
+                  <a class="event-summary__date t-center" href="${item.permalink}">
+                    <span class="event-summary__month">${item.month}</span>
+                    <span class="event-summary__day">${item.day}</span>  
+                  </a>
+                  <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="${item.permalink}">${item.title}</a></h5>
+                    <p>${item.description}<br><a href="${item.permalink}" class="nu gray"> Learn more</a></p>
+                  </div>
+                </div>
+              `).join('')}
+            </div>
+          </div>
+        `);
+        this.isSpinnerVisible = false;
+      }
+    );
+
   }
 
   keyPressDispacher(e) {
@@ -61,9 +111,12 @@ class Search {
     if (key == 27 && this.isOverlayOpen) this.closeOverlay();
   }
 
-  openOverlay() {
+  openOverlay(e) {
+    e.preventDefault();
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val('');
+    setTimeout(() => this.searchField.focus(), 301);
     this.isOverlayOpen = true;
   }
 
@@ -71,6 +124,23 @@ class Search {
     this.searchOverlay.removeClass("search-overlay--active");
     $("body").removeClass("body-no-scroll");
     this.isOverlayOpen = false;
+  }
+
+  addSearchHTML(){
+    $('body').append(`
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" id="search-term" class="search-term" placeholder="What are you looking for?">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="container">
+          <div id="search-overlay__results"></div>
+        </div>
+      </div>
+    `);
   }
 
 }
