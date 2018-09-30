34b8d111df7481b9db89b8232fc9c0fed21cdca2
Felix Avelar <felixavco@gmail.com>
2018-09-30 12:12:38 -0600

    updated 201809301212

--- functions.php
+++ functions.php
@@ -115,3 +115,27 @@
 
   add_filter('acf/fields/google_map/api', 'universityMapKey');
 
+
+  //Redirect subs accounts out admin to home page
+  add_action('admin_init', 'redirectSubsToFrontend');
+
+  function redirectSubsToFrontend() {
+
+    $currentUser =  wp_get_current_user();
+    if(count($currentUser->roles) == 1 AND $currentUser->roles[0] == 'subscriber'){
+      wp_redirect(site_url('/'));
+      exit;
+    }
+  }
+
+  add_action('wp_loaded', 'noSubsAdminBar');
+
+  function noSubsAdminBar() {
+
+    $currentUser =  wp_get_current_user();
+    if(count($currentUser->roles) == 1 AND $currentUser->roles[0] == 'subscriber'){
+      show_admin_bar( false );
+    }
+  }
+
+
